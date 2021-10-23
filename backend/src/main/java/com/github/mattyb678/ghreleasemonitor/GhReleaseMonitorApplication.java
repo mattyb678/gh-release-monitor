f4c86e949d82fb20b03b1859b91f8584fb74715c
Matt Berteaux <mattyb678@gmail.com>
2021-10-23 11:39:28 -0600

    Add directory for backend code, created with start.spring.io

---
+++ backend/src/main/java/com/github/mattyb678/ghreleasemonitor/GhReleaseMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.github.mattyb678.ghreleasemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhReleaseMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhReleaseMonitorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/mattyb678/ghreleasemonitor/GhReleaseMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mattyb678.ghreleasemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhReleaseMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
